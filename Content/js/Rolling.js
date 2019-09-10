3cb0bd490917958f3c9e7a745b4a8978b5776e14
HansKristianNielsen <hken1972@gmail.com>
2019-09-10 09:13:20 +0200

    separated dices

--- Content/js/Rolling.js
+++ Content/js/Rolling.js
@@ -1,54 +1,18 @@
-// Sætter antal af terninger
-let num_dices = 2;
-// Array til terninger
-let array_dices = [];
-// Sætter array til engelske tal - skal bruges til font awesome ikoner
-let array_dice_names = ["", "Et_hul", "To_hul", "Tre_hul", "Fire_hul", "Fem_hul", "Seks_hul"];
-// Sætter var til html element der skal vise resultater
-let display_result = document.getElementById("display_result");
-// Sætter addEventListener til klik på knap
-document.getElementById("rollthedice").addEventListener("click", rollTheDice);
-
-/**
- * Funktion til at hente tilfældigt nummer
- * @param num_eyes
- * @returns {number}
- */
-function getRandomNumber(num_eyes = 6) {
-    return Math.ceil(Math.random() * num_eyes)
-}
-
-/**
- * Funktion der nulstiller gameboard
- */
-function initGame() {
-    array_dices = [];
-    gameboard.innerHTML = "";
-
-    //Bygger array med terninger - hver terning fÃ¥r et tilfældigt nummer
-    for(let i = 1; i <= num_dices; i++) {
-        array_dices.push(getRandomNumber());
+function rollDice() {
+    const die1 = document.getElementById('die1');
+    const die2 = document.getElementById('die2');
+    const result = document.getElementById('result');
+    const d1 = Math.ceil(Math.random() * 6);
+    const d2 = Math.ceil(Math.random() * 6);
+    const diceTotal = d1 + d2;
+    die1.innerHTML = d1;
+    die2.innerHTML = d2;
+    result.innerHTML = `You rolled ${diceTotal}.`;
+    if (d1 === d2) {
+      result.innerHTML += ' DOUBLES! You get a free turn!!';
     }
-}
-
-/**
- * Funktion til at kaste terninger med
- */
-function rollTheDice() {
-    // Nulstiller spil
-    initGame();
-
-    // Lopper array
-    for(let num of array_dices) {
-        // Opretter <i> element til font awesome ikon
-        let elm = document.createElement("i");
-        // Tilføjer class attribute med font awesome klasser til element
-        elm.setAttribute("id", + array_dice_names[num]);
-        // Tilføjer element til div id gameboard
-        gameboard.appendChild(elm);
+    if (d1 === 1 && d2 === 1) {
+      result.innerHTML += ' and SNAKE EYES! You get a free turn!!';
     }
-}
-
-// Kaster terningerne når siden loades
-rollTheDice();
-
+  }
+  
